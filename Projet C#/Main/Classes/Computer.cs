198b4a010aa7f7ccc1026efc3d7103c7d20135ee
Valentin MICHUT <valentin@michut.fr>
2018-11-30 01:13:42 +0100

    Add Commentaires Computer

--- Projet C#/Main/Classes/Computer.cs
+++ Projet C#/Main/Classes/Computer.cs
@@ -7,10 +7,10 @@ namespace Main
 {
 	public class Computer
 	{
-		private String ip;
-		private String interf;
-		private String mac;
-		private String mail;
+		private String ip; //ip PC courant
+		private String interf; //nom interface sélectionnée
+		private String mac; //mac PC courant
+		private String mail; //mail sur lequel seront envoyés les rapports
 		
 		ShellConsole shell = new ShellConsole();
 		public Computer(){}
@@ -23,26 +23,31 @@ namespace Main
 		}
 		
 		public void setMail(String newMail)
+			//changement de mail
 		{
 			this.mail = newMail;
 		}	
 
 		
 		void setInterf(String interf)
+			//changement d'interface sélectionnée
 		{
 			this.interf = interf;
-			this.ip = getInfo("ip", interf); 
-			this.mac = getInfo("mac", interf); 
+			this.ip = getInfo("ip", interf);
+			this.mac = getInfo("mac", interf);
 		}
 		
 		
 		void FilesUpdate()
+			//maj des fichiers avec les interfaces et les correspondances ip/mac
 		{
 			shell.getIpMac();
 			shell.getDevice();
 		}
 		
 		String getInfo(String addr, String choix)
+			//récupération de l'ip ou de la mac selon l'argument choix:
+			//choix = "ip" sinon récupère la mac
 		{
 			FilesUpdate();	      
 
@@ -56,9 +61,9 @@ namespace Main
 	        {
 	        	if(avantavant.Contains(choix.Trim()))
 	        	{
-	        		macRetour = avant.Substring(44, 17);
+	        		macRetour = avant.Substring(44, 17); //récupération mac
 	        		string line2 = line.Substring(44);
-	        		string pattern = "\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b";
+	        		string pattern = "\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b"; //regex ip
 	        		Match m2 = Regex.Match(line2, pattern);
 	        		ipRetour = m2.Value;
 	        	}
